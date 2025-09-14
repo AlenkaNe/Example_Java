5ff684e0b435bd9f71c84a8e28e382a6ff342f1c
Alex <alexandrshirokanev@gmail.com>
2025-09-14 22:07:07 +0400

    Тест на преобразование массива в строку

--- src/test/java/JoinTest.java
+++ src/test/java/JoinTest.java
@@ -1,9 +1,15 @@
+import org.example.StringJoiner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class JoinTest {
     @Test
     protected void joinTest() {
-        Assertions.assertEquals("popo", "popo");
+        Assertions.assertEquals("Аня, Маша", StringJoiner.arrayToString(new String[] {"Аня", "Маша"}));
+    }
+
+    @Test
+    void assertNull() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> StringJoiner.arrayToString(null));
     }
 }
