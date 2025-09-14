349c50468f90d30e3c9e34dfdc8fc6d63a56ee67
Alex <alexandrshirokanev@gmail.com>
2025-09-14 21:41:00 +0400

    Тест пример

---
+++ src/test/java/Child.java
@@ -0,0 +1,5 @@
+public class Child extends JoinTest {
+    public void m() {
+        joinTest();
+    }
+}
---
+++ src/test/java/JoinTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JoinTest {
+    @Test
+    protected void joinTest() {
+        Assertions.assertEquals("popo", "popo");
+    }
+}
